729cdba7b5a323c35fa012509a39e3c1a47eedd3
Nikodimos-Developer <98909873+nikodimosewnetu@users.noreply.github.com>
2025-07-04 11:52:03 +0300

    Time: 4 ms (54.43%), Space: 13 MB (89.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,7 +1,16 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    return [i,j]
-               
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        indexed_nums={}
+        for i, num in enumerate(nums):
+            num2=target-num
+            if num2 in indexed_nums:
+                index2=indexed_nums[num2]
+                index1=i
+                return [min(index1,index2),max(index1,index2)]
+            indexed_nums[num]=i
+        return []
